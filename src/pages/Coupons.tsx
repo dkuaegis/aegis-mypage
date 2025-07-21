addcce8957a97dc4c8e018e12c4700d0594e887e
seyun31 <2ne1jenna@naver.com>
2025-07-21 17:56:56 +0900

    feat: EmptyState 추가

--- src/pages/Coupons.tsx
+++ src/pages/Coupons.tsx
@@ -2,43 +2,38 @@ import { useState } from "react";
 import Header from "../components/Header";
 import TabSelector from "../components/TabSelector";
 import Card from "../components/Card";
+import EmptyState from "../components/EmptyState";
 
 const Coupons: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState(0);
   // 더미 쿠폰 데이터
-  const couponData = [
-    {
-      id: 1,
-      price: "2000원",
-      desc: "동아리 회비 할인 쿠폰",
-      status: "사용전" as const,
-    },
-    {
-      id: 2,
-      price: "2000원",
-      desc: "동아리 회비 할인 쿠폰",
-      status: "사용후" as const,
-    },
-  ];
+  const couponData: { id: number; price: string; desc: string; status: "사용전" | "사용후" }[] = [];
+
   return (
     <div>
       <Header leftChild={"<"} title={"쿠폰함"} />
-      <TabSelector
-        tabs={["전체", "적립", "사용"]}
-        selected={selectedTab}
-        onSelect={setSelectedTab}
-      />
-      <div className="coupons-history-list">
-        {couponData.map((item) => (
-          <Card
-            key={item.id}
-            type="coupon"
-            price={item.price}
-            desc={item.desc}
-            status={item.status}
+      {couponData.length > 0 ? (
+        <>
+          <TabSelector
+            tabs={["전체", "사용전", "사용후"]}
+            selected={selectedTab}
+            onSelect={setSelectedTab}
           />
-        ))}
-      </div>
+          <div className="coupons-history-list">
+            {couponData.map((item) => (
+              <Card
+                key={item.id}
+                type="coupon"
+                price={item.price}
+                desc={item.desc}
+                status={item.status}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <EmptyState type="coupon" />
+      )}
     </div>
   );
 };
