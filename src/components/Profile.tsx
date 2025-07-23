55556138dcbd54d83462e3f189477367beb74229
seyun31 <2ne1jenna@naver.com>
2025-07-23 19:21:43 +0900

    feat: 프로필 이미지 수정 버튼 추가

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,21 @@
 import "../style/Profile.css";
+import editIcon from "../assets/edit.png";
 
 const Profile: React.FC = () => {
     return (
         <div className="Profile">
-            <div className="Profile_img">
+            <div className="profile_img" style={{ position: 'relative' }}>
                 <img src="../src/assets/C.png" alt="profile img" />
+                <img
+                    src={editIcon}
+                    alt="edit"
+                    className="profile_edit"
+                    onClick={() => {}}
+                />
             </div>
-            <div className="Profile_info">
-                <div className="Profile_name">임세윤</div>
-                <div className="Profile_greeting">
+            <div className="profile_info">
+                <div className="profile_name">임세윤</div>
+                <div className="profile_greeting">
                     환영합니다!<br />Aegis와 함께해요❤
                 </div>
             </div>
