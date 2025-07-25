bdba235853edb07937049e03af01c9f6649622a8
임세윤 <2ne1jenna@naver.com>
2025-07-25 23:20:12 +0900

    feat: 새로고침시 QRCode 재발급 api 연동 (#10)

--- src/components/QRModal.tsx
+++ src/components/QRModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { issueQRCode } from "../api/QRCode";
 import type { QRModalProps } from "../model/QRModal";
 import "../style/QRModal.css";
 import refreshImg from "../assets/refresh.png";
@@ -5,17 +7,29 @@ import closeImg from "../assets/close.png";
 import Button from "../components/Button";
 
 const QRModal: React.FC<QRModalProps> = ({ onClose, qrImageUrl }) => {
+  const [qrUrl, setQrUrl] = useState<string>(qrImageUrl || '');
+
+// QRCode 생성 API 호출
+  const onClickQR = async () => {
+    try {
+      const base64 = await issueQRCode();
+      setQrUrl(`data:image/png;base64,${base64}`);
+    } catch (e) {
+      console.error('QR 발급 실패:', e);
+    }
+  };
+
   return (
     <div className="qr-modal-overlay">
       <div className="qr-modal">
         <h2 className="qr-title">입장을 위한 QR코드</h2>
         <p className="qr-desc">이용하려는 행사에 QR코드로 체크인하세요.</p>
         <div className="qr-image-wrapper">
-          <img src={qrImageUrl} alt="QR Code" className="qr-image" />
+          <img src={qrUrl} alt="QR Code" className="qr-image" />
         </div>
         <Button
           type="REFRESH"
-          onClick={() => { /* 새로고침 동작 구현 예정 */ }}
+          onClick={onClickQR}
           text={
             <>
               <img src={refreshImg} alt="새로고침" className="qr-btn-icon" />
