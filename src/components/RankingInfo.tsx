567fb1c6b3f346e8b94100f951495e4f15c4d863
seyun31 <2ne1jenna@naver.com>
2025-09-09 20:54:14 +0900

    refactor: 랭킹페이지 갱신 시간 제거

--- src/components/RankingInfo.tsx
+++ src/components/RankingInfo.tsx
@@ -6,7 +6,6 @@ const RankingInfo: React.FC<RankingInfoProps> = ({ totalParticipants }) => {
     <div className="ranking-info-container">
       <p className="participants-text">총 {totalParticipants}명 참여중</p>
       <h1 className="ranking-title">TOP 10</h1>
-      <p className="ranking-time-text">매일 오전 03:00 갱신</p>
     </div>
   );
 };
