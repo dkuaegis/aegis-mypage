dedb8df7a1d08efd743c1db0f7a0f526cb1923fb
seyun31 <2ne1jenna@naver.com>
2025-11-11 17:19:48 +0900

    feat: Goolge Analytics 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { initGoogleAnalytics } from './utils/analytics'
+
+// Google Analytics 초기화
+initGoogleAnalytics()
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
